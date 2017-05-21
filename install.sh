3a584f35a6e9c49c612e61032115311cc0220173
Python3pkg <raliclo@gmail.com>
2017-05-21 04:46:07 -0700

    Update travis

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install python-pdfminer
+sudo pip install -U lxml
+sudo pip install numpy==1.6.2
+sudo pip install pyyaml nltk==3.0.0
+sudo pip install pyenchant==1.6.5
+sudo pip install pattern==2.6
